69ece77de750a996b0267144a63504e6c7790045
Tráng Hà Viết <viettrangha@gmail.com>
2017-07-02 23:29:15 +0700

    initialize controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'carts/edit'
+
+  get 'carts/update'
+
+  get 'carts/destroy'
+
+  get 'book/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
