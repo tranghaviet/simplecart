e020808f7876fd5d9a48f6fa7690a1535170ae54
Tráng Hà Viết <viettrangha@gmail.com>
2017-07-06 12:16:38 +0700

    install RSpec

--- spec/models/cart_book_spec.rb
+++ spec/models/cart_book_spec.rb
@@ -1,5 +1,12 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe CartBook, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "cart existed and cart_book does not exist" do
+   it "make a new cart_book" do
+     cart = Cart.create!(session_id: "session_id")
+     cart_book = CartBook.create!(cart_id: cart.id, book_id: 1, quantity: 1)
+     
+     expect(CartBook.findby(cart_id: cart.id, book_id: 1)).to eq(cart_book)
+   end
+  end
 end
